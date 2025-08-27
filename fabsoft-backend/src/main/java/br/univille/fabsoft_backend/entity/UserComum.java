dd847dc860b56f3d795b75539829b06307e3c380
Satty Faria Schadeck <89159750+styFariaS@users.noreply.github.com>
2025-08-27 01:02:20 +0000

    adicionado uma classe

---
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/UserComum.java
@@ -0,0 +1,37 @@
+package br.univille.fabsoft_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserComum {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long matricula;
+    private String nomeCompleto;
+    private String contato;
+
+    
+    public long getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    public String getContato() {
+        return contato;
+    }
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    
+}
