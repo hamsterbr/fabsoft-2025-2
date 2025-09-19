2b3cb1ead1e3f24e0b7b36bc6ce53cf18450136c
Satty Faria Schadeck <89159750+styFariaS@users.noreply.github.com>
2025-09-19 00:27:58 +0000

    Controller de Solicitacao atualizado

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/controller/SolicitacaoController.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/controller/SolicitacaoController.java
@@ -1,10 +1,12 @@
 package br.univille.fabsoft_backend.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.univille.fabsoft_backend.entity.Solicitacao;
 import br.univille.fabsoft_backend.service.SolicitacaoService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/api/v1/solicitacoes")
@@ -29,6 +33,26 @@ public class SolicitacaoController {
         
         return new ResponseEntity<List<Solicitacao>>(listaSolicitacoes, HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<Solicitacao> save(@Valid @RequestBody Solicitacao solicitacao, BindingResult result){
+        if (solicitacao == null){
+            return ResponseEntity.badRequest().build();
+        }
+        if (result.hasErrors()) {
+            HttpHeaders headers = new HttpHeaders();
+            String errorMessages = result.getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(" "));
+                return new ResponseEntity<Solicitacao>(solicitacao, headers, HttpStatus.BAD_REQUEST);
+        }
+        if (solicitacao.getId() == 0) {
+            solicitacao = service.save(solicitacao);
+            return new ResponseEntity<Solicitacao>(solicitacao, HttpStatus.OK);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+    
 
     @PutMapping("/{id}")
     public ResponseEntity<Solicitacao> update(@RequestBody Solicitacao solicitacao, @PathVariable long id) {
